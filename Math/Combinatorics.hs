3a981efaac181262cba53c1f4b007931519f5394
Menachem Dov Mostowicz <menachem@gesh.uni.cx>
2020-01-25 20:18:44 +0200

    Add conjenctural formulae

--- Math/Combinatorics.hs
+++ Math/Combinatorics.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE TupleSections #-}
+
 module Math.Combinatorics
   ( both
   , choose
+  , decomps
   , doubleFactorial
   , factorial
+  , ive
   , multiFactorial
   , matching
   , std
@@ -15,12 +19,37 @@ import           Math.Combinat.Partitions.Integer
                                                 , fromPartition
                                                 )
 
+import           Data.Function                  ( on )
+import           Data.List                      ( groupBy
+                                                , sortBy
+                                                )
+import           Control.Applicative            ( liftA2 )
+
+type Equiv a = [[a]]
+
+decomps :: (Num a, Eq a, Show a) => Int -> (Equiv a -> Bool) -> [a] -> Int
+decomps n p ws = length $ filter p (n `colorings` ws)
+
+-- | probably the wrong function, actually want just partitions?
+colorings :: Int -> [a] -> [Equiv a]
+colorings n xs =
+  [ map (map fst) $ groupUnsortedOn snd cxs | cxs <- n `colors` xs ]
+  where groupUnsortedOn f = groupBy ((==) `on` f) . sortBy (compare `on` f)
+
+-- | @colors n xs@ gives the $n$-colorations of `xs`
+colors :: Int -> [a] -> [[(a, Int)]]
+colors _ []       = [[]]
+colors n (x : xs) = liftA2 (:) (map (x, ) (std n)) (colors n xs)
+
 choose :: [a] -> Int -> [[a]]
 choose (x : xs) n | n > 0 = map (x :) (xs `choose` (n - 1)) ++ xs `choose` n
 choose xs _               = [ [] | null xs ]
 
 std :: (Num a, Enum a) => a -> [a]
 std n = [0 .. n - 1]
+
+ive :: Num a => Bool -> a
+ive p = if p then 1 else 0
 
 both :: (a -> b) -> (a, a) -> (b, b)
 both f (x, y) = (f x, f y)
--- Trace.hs
+++ Trace.hs
@@ -1,12 +1,15 @@
 module Trace
   ( eqns
   , stolz
+  , gesh
   , table
   )
 where
 
 import           Math.Combinatorics             ( both
                                                 , choose
+                                                , decomps
+                                                , ive
                                                 , std
                                                 , matching
                                                 , weight
@@ -28,17 +31,18 @@ expNot = map collapseOccs . group . fromPartition
   where collapseOccs xs = (length xs, fromMaybe 1 $ listToMaybe xs)
 
 eqns :: [Int -> Partition -> Either Int Int]
-stolz, table :: Int -> Partition -> Either Int Int
-eqns@(stolz : table : _) = map
+stolz, gesh, table :: Int -> Partition -> Either Int Int
+eqns@(stolz : gesh : table : _) = map
   (\(p, f) -> curry $ liftA2 s2p p (uncurry f))
   [ (\(n, l) -> weight l <= 2 * n, stolz')
+  , (\(n, _) -> n <= 3           , gesh')
   , ((`elem` map fst known), curry $ fromMaybe 0 . flip lookup known)
   ]
  where
   s2p True  = Right
   s2p False = Left
 
-stolz' :: Int -> Partition -> Int
+stolz', gesh' :: Int -> Partition -> Int
 
 stolz' = const $ product . map f . expNot
  where
@@ -57,6 +61,32 @@ stolz' = const $ product . map f . expNot
 
   n `c` k = fromIntegral $ length (std n `choose` k)
   offsets j a = j ^ (a `quot` 2)
+
+gesh' 1 l = ive . even $ weight l
+gesh' 2 l =
+  let l' = fromPartition l
+  in  (ive . even $ weight l)
+        *      ( decomps 2 (all ((== (weight l `quot` 2)) . sum)) l'
+               + (ive (all (\(a, j) -> (a > 0) |- even j) es) * product
+                   (map ((2 ^) . fst) es)
+                 )
+               )
+        `quot` 2
+ where
+  es = expNot l
+  p |- q = not p || q
+gesh' 3 l =
+  let l' = fromPartition l
+  in  2
+        * decomps 2 ((`ofPat` [2 `n` 0, 2 `n` 0, 2 `n` 0]) . map sum)    l'
+        - decomps 3 ((`ofPat` [1 `n` (-1), 2 `n` 0, 2 `n` 1]) . map sum) l'
+        - decomps 3 ((`ofPat` [1 `n` 0, 1 `n` (-1), 2 `n` 1]) . map sum) l'
+ where
+  n :: (Integral a, Ord a) => a -> a -> a -> Bool
+  d `n` a = \x -> (x - a) >= 0 && ((x - a) `mod` d == 0)
+  ofPat :: [a] -> [a -> Bool] -> Bool
+  ofPat = (and .) . zipWith (flip ($))
+gesh' _ _ = -1 -- an impossible value
 
 known :: [((Int, Partition), Int)]
 known =
